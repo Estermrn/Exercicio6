35e16c88d460691969004fcfde9fd9217ae3a8d2
daros <daros@EsterMariani>
2025-04-09 11:27:40 -0300

    Inicio do projeto

---
+++ src/main/java/com/mycompany/exercicio6/Exercicio6.java
@@ -0,0 +1,13 @@
+package com.mycompany.exercicio6;
+
+import javax.swing.JOptionPane;
+
+public class Exercicio6 {
+
+    public static void main(String[] args) {
+
+        int variavel = 20;
+
+        JOptionPane.showMessageDialog(null, "O valor da variável é : " + variavel);
+    }
+}
